fdd36d53320c21dd2de341124d08c4659a0e1866
Alexis SARRABAYROUSE <alexis.sarrabayrouse@gmail.com>
2018-02-04 18:47:59 +0100

    interpo

--- domain/domain.c
+++ domain/domain.c
@@ -9,37 +9,18 @@ unsigned short bilinear_interpolation(float x, float y, pnm ims, int c) {
     int j = x;
     float dx = x - j;
     float dy = y - i;
-    unsigned short interpolation;
-    if ((i >= 0) && (j >= 0) && (i < rows) && (j < cols)) {
-        interpolation = (1-dx) * (1-dy) * pnm_get_component(ims, i, j, c);
-        if ((i + 1 < rows) && (j + 1 < cols)) {
-            return interpolation + dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
-            + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
-            + dx * dy * pnm_get_component(ims, i+1, j+1, c);
-        }
-        else if ((i + 1 < rows) && (j + 1 >= cols)) {
-            return interpolation + (1-dx) * dy * pnm_get_component(ims, i+1, j, c);
-        }
-        else if ((i + 1 >= rows) && (j + 1 < cols)) {
-            return interpolation + dx * (1-dy) * pnm_get_component(ims, i, j+1, c);
-        }
+    unsigned short interpolation = 0;
+    if (i >= 0 && j >= 0 && i < rows && j < cols) {
+        interpolation = interpolation + (1-dx) * (1-dy) * pnm_get_component(ims, i, j, c);
     }
-    else if ((i >= 0) && (j < 0) && (j + 1 >= 0) && (i < rows) && (j< cols)) {
-        if (i + 1 < rows) {
-            return dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
-            + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
-            + dx * dy * pnm_get_component(ims, i+1, j+1, c);
-        }
+    if (i >= 0 && j+1 >= 0 && i < rows && j+1 < cols) {
+        interpolation = interpolation + dx * (1-dy) * pnm_get_component(ims, i, j+1, c);
     }
-    else if ((i < 0) && (i + 1 >= 0) && (j >= 0) && (i < rows) && (j < cols)) {
-        if (j + 1 < cols) {
-            return dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
-            + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
-            + dx * dy * pnm_get_component(ims, i+1, j+1, c);
-        }
+    if (i+1 >= 0 && j >= 0 && i+1 < rows && j < cols) {
+        interpolation = interpolation + (1-dx) * dy * pnm_get_component(ims, i+1, j, c);
     }
-    else if ((i < 0) && (j < 0) && (i + 1 >= 0) && (j + 1 >= 0)) {
-        return dx * dy * pnm_get_component(ims, i+1, j+1, c);
+    if (i+1 >= 0 && j+1 >= 0 && i+1 < rows && j+1 < cols) {
+        interpolation = interpolation + dx * dy * pnm_get_component(ims, i+1, j+1, c);
     }
-    return 0;
+    return interpolation;
 }
--- domain/rotate.c
+++ domain/rotate.c
@@ -1,24 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pnm.h>
+#include <math.h>
+#include <domain.h>
 
 
 
-void process(int x, int y, float angle, char *ims_name, char *imd_name){
+void process(int x_center, int y_center, float angle, char *ims_name, char *imd_name){
     pnm ims = pnm_load(ims_name);
     int rows = pnm_get_height(ims);
     int cols = pnm_get_width(ims);
     pnm imd = pnm_new(cols, rows, PnmRawPpm);
 
-    for (size_t i = 0; i < rows; i++) {
-        for (size_t j = 0; j < cols; j++) {
-            float xp = cos(angle) * (j-x) + sin(angle) * (i-y) + x;
-            float yp = -sin(anlge) * (j-x) + cos(angle) * (i-y) + y;
-            for (size_t k = 0; k < 3; k++) {
-                
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            float x = cos(angle) * (j-x_center) - sin(angle) * (i-y_center) + x_center;
+            float y = sin(angle) * (j-x_center) + cos(angle) * (i-y_center) + y_center;
+            // printf("x : %5f | y : %5f\n",x, y );
+            for (int c = 0; c < 3; c++) {
+                pnm_set_component(imd, i, j, c, bilinear_interpolation(x, y, ims, c));
             }
         }
     }
+
+    pnm_save(imd, PnmRawPpm, imd_name);
 }
 
 void usage(char* s) {
