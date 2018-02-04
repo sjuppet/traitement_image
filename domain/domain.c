96863443e33253c762f56e0e446f2a2d37d50bf2
sjuppet <sophie.juppet@laposte.net>
2018-02-04 17:13:38 +0100

    fixed interpolation

--- domain/domain.c
+++ domain/domain.c
@@ -2,14 +2,24 @@
 
 #include <domain.h>
 
-unsigned short bilinear_interpolation(float x, float y, pnm ims, int c){
+unsigned short bilinear_interpolation(float x, float y, pnm ims, int c) {
+    int rows = pnm_get_height(ims);
+    int cols = pnm_get_width(ims);
     int i = y;
     int j = x;
     float dx = x - j;
     float dy = y - i;
-    return
-        (1-dx) * (1-dy) * pnm_get_component(ims, i, j, c)
-        + dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
-        + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
-        + dx * dy * pnm_get_component(ims, i+1, j+1, c);
+    unsigned short interpolation = (1-dx) * (1-dy) * pnm_get_component(ims, i, j, c);
+    if (i + 1 >= rows) {
+        interpolation = interpolation + dx * (1-dy) * pnm_get_component(ims, i, j+1, c);
+    }
+    if (j + 1 >= cols) {
+        interpolation = interpolation + (1-dx) * dy * pnm_get_component(ims, i+1, j, c);
+    }
+    else {
+        interpolation = interpolation + dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
+                                      + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
+                                      + dx * dy * pnm_get_component(ims, i+1, j+1, c);
+    }
+    return interpolation;
 }
--- domain/shear.c
+++ domain/shear.c
@@ -6,76 +6,76 @@
 #include "domain.h"
 
 void process(char * dir, int teta, char * ims_name, char * imd_name) {
-  pnm ims = pnm_load(ims_name);
-  int rows = pnm_get_height(ims);
-  int cols = pnm_get_width(ims);
+    pnm ims = pnm_load(ims_name);
+    int rows = pnm_get_height(ims);
+    int cols = pnm_get_width(ims);
 
-  pnm imd = pnm_new(cols, rows, PnmRawPpm);
-  unsigned short * data = pnm_get_image(imd);
-  size_t size = rows*cols*3;
-  while(size--) *data++ = 0;
+    pnm imd = pnm_new(cols, rows, PnmRawPpm);
+    unsigned short * data = pnm_get_image(imd);
+    size_t size = rows*cols*3;
+    while(size--) *data++ = 0;
 
-  float t = tanf(teta);
+    float t = tanf(teta);
 
-  if (dir[0] == 'h') {
-    /*
-    (x', y') = (x + y*tan(teta), y)
-    (x, y) = (x' - y'*tan(teta), y')
-    */
-    for (int i = 0; i < rows - 1; i++) {
-      for (int j = 0; j < cols - 1; j++) {
-        for (int k = 0; k < 3; k++) {
-          pnm_set_component(imd, i, j, k, bilinear_interpolation(j - i*t, i, ims, k));
+    if (dir[0] == 'h') {
+        /*
+        (x', y') = (x + y*tan(teta), y)
+        (x, y) = (x' - y'*tan(teta), y')
+        */
+        for (int i = 0; i < rows - 1; i++) {
+            for (int j = 0; j < cols - 1; j++) {
+                for (int k = 0; k < 3; k++) {
+                    pnm_set_component(imd, i, j, k, bilinear_interpolation(j - i*t, i, ims, k));
+                }
+            }
         }
-      }
     }
-  }
-  if (dir[0] == 'v') {
-    /*
-    (x', y') = (x, y + x*tan(teta))
-    (x, y) = (x', y' - x'*tan(teta))
-    */
-    for (int i = 0; i < rows - 1; i++) {
-      for (int j = 0; j < cols - 1; j++) {
-        for (int k = 0; k < 3; k++) {
-          pnm_set_component(imd, i, j, k, bilinear_interpolation(j, i - j*t, ims, k));
+    if (dir[0] == 'v') {
+        /*
+        (x', y') = (x, y + x*tan(teta))
+        (x, y) = (x', y' - x'*tan(teta))
+        */
+        for (int i = 0; i < rows - 1; i++) {
+            for (int j = 0; j < cols - 1; j++) {
+                for (int k = 0; k < 3; k++) {
+                    pnm_set_component(imd, i, j, k, bilinear_interpolation(j, i - j*t, ims, k));
+                }
+            }
         }
-      }
     }
-  }
-  else {
-    printf("First argument is not a valid direction\n");
-    exit(EXIT_FAILURE);
-  }
+    else {
+        printf("First argument is not a valid direction\n");
+        exit(EXIT_FAILURE);
+    }
 
-  pnm_save(imd, PnmRawPpm, imd_name);
+    pnm_save(imd, PnmRawPpm, imd_name);
 
-  pnm_free(imd);
-  pnm_free(ims);
+    pnm_free(imd);
+    pnm_free(ims);
 }
 
 static void usage(char* s){
-  fprintf(stderr,"%s <dir>{h ,v} <angle> <ims> <imd>\n",s);
-  exit(EXIT_FAILURE);
+    fprintf(stderr,"%s <dir>{h ,v} <angle> <ims> <imd>\n",s);
+    exit(EXIT_FAILURE);
 }
 
 #define PARAM 4
 int main(int argc, char* argv[]){
-  if(argc != PARAM+1)
+    if(argc != PARAM+1)
     usage(argv[0]);
 
-  char * dir = argv[1];
-  int teta = atoi(argv[2]);
-  char * ims_name = argv[3];
-  char * imd_name = argv[4];
+    char * dir = argv[1];
+    int teta = atoi(argv[2]);
+    char * ims_name = argv[3];
+    char * imd_name = argv[4];
 
-  process(dir, teta, ims_name, imd_name);
-  /*
-     just for the test, completely useless for the shear program
-  for(int channel=0; channel<3; channel++){
+    process(dir, teta, ims_name, imd_name);
+    /*
+    just for the test, completely useless for the shear program
+    for(int channel=0; channel<3; channel++){
     unsigned short v = bilinear_interpolation(0.0, 0.0, NULL, channel);
     (void)v;
-  }
-  */
-  return EXIT_SUCCESS;
+}
+*/
+return EXIT_SUCCESS;
 }
