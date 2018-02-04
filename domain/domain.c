bb5c76e891f5924d9974170458008f71f7b015b3
sjuppet <sophie.juppet@laposte.net>
2018-02-04 18:02:52 +0100

    interpolation corrected

--- domain/domain.c
+++ domain/domain.c
@@ -13,36 +13,33 @@ unsigned short bilinear_interpolation(float x, float y, pnm ims, int c) {
     if ((i >= 0) && (j >= 0) && (i < rows) && (j < cols)) {
         interpolation = (1-dx) * (1-dy) * pnm_get_component(ims, i, j, c);
         if ((i + 1 < rows) && (j + 1 < cols)) {
-            interpolation = interpolation + dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
-                                          + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
-                                          + dx * dy * pnm_get_component(ims, i+1, j+1, c);
+            return interpolation + dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
+            + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
+            + dx * dy * pnm_get_component(ims, i+1, j+1, c);
         }
         else if ((i + 1 < rows) && (j + 1 >= cols)) {
-            interpolation = interpolation + (1-dx) * dy * pnm_get_component(ims, i+1, j, c);
+            return interpolation + (1-dx) * dy * pnm_get_component(ims, i+1, j, c);
         }
         else if ((i + 1 >= rows) && (j + 1 < cols)) {
-            interpolation = interpolation + dx * (1-dy) * pnm_get_component(ims, i, j+1, c);
+            return interpolation + dx * (1-dy) * pnm_get_component(ims, i, j+1, c);
         }
     }
     else if ((i >= 0) && (j < 0) && (j + 1 >= 0) && (i < rows) && (j< cols)) {
         if (i + 1 < rows) {
-            interpolation = dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
-                          + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
-                          + dx * dy * pnm_get_component(ims, i+1, j+1, c);
+            return dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
+            + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
+            + dx * dy * pnm_get_component(ims, i+1, j+1, c);
         }
     }
     else if ((i < 0) && (i + 1 >= 0) && (j >= 0) && (i < rows) && (j < cols)) {
         if (j + 1 < cols) {
-            interpolation = dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
-                          + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
-                          + dx * dy * pnm_get_component(ims, i+1, j+1, c);
+            return dx * (1-dy) * pnm_get_component(ims, i, j+1, c)
+            + (1-dx) * dy * pnm_get_component(ims, i+1, j, c)
+            + dx * dy * pnm_get_component(ims, i+1, j+1, c);
         }
     }
     else if ((i < 0) && (j < 0) && (i + 1 >= 0) && (j + 1 >= 0)) {
-        interpolation = dx * dy * pnm_get_component(ims, i+1, j+1, c);
+        return dx * dy * pnm_get_component(ims, i+1, j+1, c);
     }
-    else {
-        interpolation = 0;
-    }
-    return interpolation;
+    return 0;
 }
