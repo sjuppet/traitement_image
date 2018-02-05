ea820961aa25e3045ce29f2a6e8de692144bd2da
Alexis SARRABAYROUSE <alexis.sarrabayrouse@gmail.com>
2018-02-05 17:02:33 +0100

    produit mat vect

--- color-transfer/color-transfer.c
+++ color-transfer/color-transfer.c
@@ -1,11 +1,11 @@
 /**
- * @file color-transfert
- * @brief transfert color from source image to target image.
- *        Method from Reinhard et al. :
- *        Erik Reinhard, Michael Ashikhmin, Bruce Gooch and Peter Shirley,
- *        'Color Transfer between Images', IEEE CGA special issue on
- *        Applied Perception, Vol 21, No 5, pp 34-41, September - October 2001
- */
+* @file color-transfert
+* @brief transfert color from source image to target image.
+*        Method from Reinhard et al. :
+*        Erik Reinhard, Michael Ashikhmin, Bruce Gooch and Peter Shirley,
+*        'Color Transfer between Images', IEEE CGA special issue on
+*        Applied Perception, Vol 21, No 5, pp 34-41, September - October 2001
+*/
 
 #include <float.h>
 #include <math.h>
@@ -16,27 +16,45 @@
 
 #define D 3
 
-float RGB2LMS[D][D] = {
-  {0.3811, 0.5783, 0.0402},
-  {0.1967, 0.7244, 0.0782},
-  {0.0241, 0.1288, 0.8444}
+static float RGB2LMS[D][D] = {
+    {0.3811, 0.5783, 0.0402},
+    {0.1967, 0.7244, 0.0782},
+    {0.0241, 0.1288, 0.8444}
 };
 
+static float LMS2RGB[D][D] = {
+    {4.4679, -3.5873, 0.1193},
+    {-1.2186, 2.3809, -0.1624},
+    {0.0497, -0.2439, 1.2045}
+};
+
+/**
+ * produit matrice vecteur dans res
+ */
+static void prod_mat_vect(float mat[D][D], float vect[D], float res[D]){
+    for (size_t i = 0; i < D; i++) {
+        res[i] = 0;
+        for (size_t j = 0; j < D; j++) {
+            res[i] += mat[i][j] * vect[i];
+        }
+    }
+}
+
 void process(char *ims, char *imt, char* imd){
-  (void) ims;
-  (void) imt;
-  (void) imd;
+    (void) ims;
+    (void) imt;
+    (void) imd;
 
 }
 
 void usage (char *s){
-  fprintf(stderr, "Usage: %s <ims> <imt> <imd> \n", s);
-  exit(EXIT_FAILURE);
+    fprintf(stderr, "Usage: %s <ims> <imt> <imd> \n", s);
+    exit(EXIT_FAILURE);
 }
 
 #define PARAM 3
 int main(int argc, char *argv[]){
-  if (argc != PARAM+1) usage(argv[0]);
-  process(argv[1], argv[2], argv[3]);
-  return EXIT_SUCCESS;
+    if (argc != PARAM+1) usage(argv[0]);
+    process(argv[1], argv[2], argv[3]);
+    return EXIT_SUCCESS;
 }
