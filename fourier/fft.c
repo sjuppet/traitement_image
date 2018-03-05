0006495b4ec34d580157b10ca3cc0a132120f03f
sjuppet <sophie.juppet@laposte.net>
2018-03-05 17:32:30 +0100

    forward/backward

--- fourier/fft.c
+++ fourier/fft.c
@@ -7,23 +7,41 @@
 fftw_complex *
 forward(int rows, int cols, unsigned short* g_img)
 {
-  (void)rows;
-  (void)cols;
-  (void)g_img;
-  return NULL;
+  int size = rows * cols;
+  fftw_complex * c_in = malloc(size * sizeof(fftw_complex));
+  fftw_complex * c_out = malloc(size * sizeof(fftw_complex));
+
+  for (int i = 0; i < size; i++) {
+    c_in[i] = g_img[i] + I * 0;
+  }
+
+  fftw_plan plan = fftw_plan_dft_2d(rows, cols, c_in, c_out, FFTW_FORWARD, FFTW_ESTIMATE);
+  fftw_execute(plan);
+
+  fftw_destroy_plan(plan);
+  free(c_in);
+  return c_out;
 }
 
-unsigned short *
-backward(int rows, int cols, fftw_complex* freq_repr)
-{
-  (void)rows;
-  (void)cols;
-  (void)freq_repr;
-  return NULL;
+unsigned short * backward(int rows, int cols, fftw_complex * freq_repr) {
+  int size = rows * cols;
+  fftw_complex * c_out = malloc(size * sizeof(fftw_complex));
+  unsigned short * img = malloc(size * sizeof(unsigned short));
+
+  fftw_plan plan = fftw_plan_dft_2d(rows, cols, freq_repr, c_out, FFTW_BACKWARD, FFTW_ESTIMATE);
+  fftw_execute(plan);
+
+  for (int i = 0; i < size; i++) {
+    img[i] = creal(c_out[i]) / size;
+  }
+
+  fftw_destroy_plan(plan);
+  free(c_out);
+  return img;
 }
 
 void
-freq2spectra(int rows, int cols, fftw_complex* freq_repr, float* as, float* ps) 
+freq2spectra(int rows, int cols, fftw_complex* freq_repr, float* as, float* ps)
 {
   (void)rows;
   (void)cols;
@@ -32,7 +50,7 @@ freq2spectra(int rows, int cols, fftw_complex* freq_repr, float* as, float* ps)
   (void)ps;
 }
 
-void 
+void
 spectra2freq(int rows, int cols, float* as, float* ps, fftw_complex* freq_repr)
 {
   (void)rows;
--- fourier/test-fft.c
+++ fourier/test-fft.c
@@ -5,6 +5,7 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <fft.h>
 
@@ -16,7 +17,32 @@ void
 test_forward_backward(char* name)
 {
   fprintf(stderr, "test_forward_backward: ");
-  (void)name;
+
+  pnm img = pnm_load(name);
+  int rows = pnm_get_height(img);
+  int cols = pnm_get_width(img);
+  unsigned short * data_in = pnm_get_image(img);
+  //char * FB_name = "";
+
+  fftw_complex * transition = forward(rows, cols, data_in);
+  unsigned short * data_out = backward(rows, cols, transition);
+
+  for (int i = 0; i < rows * cols; i++) {
+        data_in[i] = data_out[i];
+  }
+/*
+  int i = strlen(name) - 1;
+  char * a = "";
+  while (name[i] != '/') {
+    strcat(a, FB_name);
+  }
+  strcat("FB-", FB_name);
+*/
+  pnm_save(img, PnmRawPpm, "FB_name.ppm");
+
+  free(transition);
+  free(data_out);
+  free(img);
   fprintf(stderr, "OK\n");
 }
 
@@ -46,7 +72,7 @@ test_display(char* name)
 
 /**
  * @brief test the modification of magnitude by adding a periodic functions
-          on both vertical and horizontal axis, and 
+          on both vertical and horizontal axis, and
  *        construct output images
  * @param char* name, the input image file name
  */
@@ -68,7 +94,7 @@ run(char* name)
   test_add_frequencies(name);
 }
 
-void 
+void
 usage(const char *s)
 {
   fprintf(stderr, "Usage: %s <ims> \n", s);
@@ -76,7 +102,7 @@ usage(const char *s)
 }
 
 #define PARAM 1
-int 
+int
 main(int argc, char *argv[])
 {
   if (argc != PARAM+1) usage(argv[0]);
