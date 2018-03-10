59fe4b9208f3ba4cf4d2e0fe798252e18f41fd57
CALANDRA Josephine <jcalandra001@janetleight.pedago.ipb.fr>
2018-03-10 17:26:16 +0100

    affichage de noms correct pour les images

--- fourier/test-fft.c
+++ fourier/test-fft.c
@@ -6,13 +6,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <libgen.h>
 #include <fft.h>
 
 /**
  * @brief test the forward and backward functions
  * @param char* name, the input image file name
  */
+
 void
 test_forward_backward(char* name)
 {
@@ -36,14 +37,16 @@ test_forward_backward(char* name)
   for (int k = 0; k < 3; k++) {
     pnm_set_channel(img, data_out, k);
   }
-
   //int i = strlen(name) - 1;
   //while ((name[i] != '/') && (i > 0)) {
   //  i--;
   //}
   //FB_name = strcat(FB_name, name + i);
-
-  pnm_save(img, PnmRawPpm, "FB-name.ppm");
+  char filename[50], FB_name[50];
+  strcpy(filename, basename(name));
+  strcpy(FB_name, "FB-");
+  strcat(FB_name, filename);
+  pnm_save(img, PnmRawPpm, FB_name);
 
   free(transition);
   free(data_out);
@@ -91,8 +94,11 @@ test_reconstruction(char* name)
   //    i--;
   //  }
   //  FB_name = strcat(FB_name, name + i);
-
-    pnm_save(img, PnmRawPpm, "FB-ASPS-name.ppm");
+    char filename[50], ASPS_name[50];
+    strcpy(filename, basename(name));
+    strcpy(ASPS_name, "ASPS-");
+    strcat(ASPS_name, filename);
+    pnm_save(img, PnmRawPpm, ASPS_name);
 
     free(freq_repr_out);
     free(freq_repr_in);
@@ -113,7 +119,52 @@ void
 test_display(char* name)
 {
   fprintf(stderr, "test_display: ");
-  (void)name;
+
+/*
+  pnm img = pnm_load(name);
+  int rows = pnm_get_height(img);
+  int cols = pnm_get_width(img);
+  unsigned short * data_in = pnm_get_channel(img, NULL, 0);
+
+  for (int i = 0; i < rows * cols; i++) {
+    if(i%2 != 0){
+      data_in = -1 * data_in;
+    }
+  }
+
+  float * as = malloc(rows * cols * sizeof(float));
+  float * ps = malloc(rows * cols * sizeof(float));
+  fftw_complex * freq_repr_in;
+
+  freq_repr_in = forward(rows, cols, data_in);
+  freq2spectra(rows, cols, freq_repr_in, as, ps);
+  for (int i = 0; i < rows * cols; i++) {
+    as[i] = log(1 + as[i]);
+  }
+
+  pnm img_freq = pnm_dup(img);
+  pnm img_amp = pnm_dup(img);
+
+  for (int i = 0; i < rows * cols; i++) {
+    for (int k = 0; k < 3; k++) {
+      pnm_set_channel(img_freq, ps, k);
+      pnm_set_channel(img_amp, as, k);
+    }
+  }
+
+  pnm_save(img_freq, PnmRawPpm, "PS-name.ppm");
+  pnm_save(img_amp, PnmRawPpm, "AS-name.ppm");
+
+  pnm_free(img_amp);
+  pnm_free(img_freq);
+  free(freq_repr_in);
+  free(ps);
+  free(as);
+  free(data_in);
+  pnm_free(img);
+
+*/
+(void)name;
   fprintf(stderr, "OK\n");
 }
 
