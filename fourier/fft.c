8893b1c625bb3ce870b63c655336e64c018d805b
CALANDRA Josephine <jcalandra001@belote.pedago.ipb.fr>
2018-03-10 18:56:45 +0100

    gestion du recentrage de la phase et de l'amplitude

--- fourier/fft.c
+++ fourier/fft.c
@@ -6,13 +6,30 @@
 
 #define PI 3.14159265359
 
+void shift(int rows, int cols, float *buffer){
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      if((i+j)%2!=0){
+        buffer[i*cols + j] = - buffer[i*cols +j];
+      }
+    }
+  }
+}
+
 fftw_complex * forward(int rows, int cols, unsigned short* g_img) {
   int size = rows * cols;
+  float *float_g_img = malloc(size * sizeof(float));
+  for (int i = 0; i < size; i++) {
+    float_g_img[i] = g_img[i];
+  }
+
+  shift(rows, cols, float_g_img);
+
   fftw_complex * c_in = malloc(size * sizeof(fftw_complex));
   fftw_complex * c_out = malloc(size * sizeof(fftw_complex));
 
   for (int i = 0; i < size; i++) {
-    c_in[i] = g_img[i] + I * 0;
+    c_in[i] = float_g_img[i] + I * 0;
   }
 
   fftw_plan plan = fftw_plan_dft_2d(rows, cols, c_in, c_out, FFTW_FORWARD, FFTW_ESTIMATE);
@@ -20,22 +37,36 @@ fftw_complex * forward(int rows, int cols, unsigned short* g_img) {
 
   fftw_destroy_plan(plan);
   free(c_in);
+  free(float_g_img);
   return c_out;
 }
 
 unsigned short * backward(int rows, int cols, fftw_complex * freq_repr) {
   int size = rows * cols;
   fftw_complex * c_out = malloc(size * sizeof(fftw_complex));
+  float *float_img = malloc(size * sizeof(float));
   unsigned short * img = malloc(size * sizeof(unsigned short));
 
   fftw_plan plan = fftw_plan_dft_2d(rows, cols, freq_repr, c_out, FFTW_BACKWARD, FFTW_ESTIMATE);
   fftw_execute(plan);
 
   for (int i = 0; i < size; i++) {
-    img[i] = creal(c_out[i]) / size;
+    float_img[i] = creal(c_out[i]) / size;
+  }
+  shift(rows, cols, float_img);
+
+  for (int i = 0; i < size; i++) {
+    if(float_img[i] > pnm_maxval){
+      float_img[i] = pnm_maxval;
+    }
+    if(float_img[i] < 0){
+      float_img[i] = 0;
+    }
+    img[i] = (unsigned short) float_img[i];
   }
 
   fftw_destroy_plan(plan);
+  free(float_img);
   free(c_out);
   return img;
 }
